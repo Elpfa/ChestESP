80d42145c4cf6045abd44eeaae2c5c9270c2c1c8
Elpfa <50383074+Elpfa@users.noreply.github.com>
2022-03-21 20:14:40 -0230

    Update OrthoxiaChestESP.lua

--- OrthoxiaChestESP.lua
+++ OrthoxiaChestESP.lua
@@ -168,34 +168,3 @@ chests.ChildRemoved:Connect(function(chest)
     end
     print("There are currently ".. count .. " chests in the server.");
 end)
-
-local function update(input)
-    local delta = input.Position - dragStart
-    gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-end
-
-gui.InputBegan:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-        dragging = true
-        dragStart = input.Position
-        startPos = gui.Position
-
-        input.Changed:Connect(function()
-            if input.UserInputState == Enum.UserInputState.End then
-                dragging = false
-            end
-        end)
-    end
-end)
-
-gui.InputChanged:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-        dragInput = input
-    end
-end)
-
-UserInputService.InputChanged:Connect(function(input)
-    if input == dragInput and dragging then
-        update(input)
-    end
-end)
